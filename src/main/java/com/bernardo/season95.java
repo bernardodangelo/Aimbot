ec168ce301c98a1c44fc2b2852b5eba263286abe
Bernardo D'Angelo <bernardodangelo8@gmail.com>
2023-09-29 19:17:39 -0300

    initial commit

---
+++ src/main/java/com/bernardo/season95.java
@@ -0,0 +1,17 @@
+package com.bernardo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class season95 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
