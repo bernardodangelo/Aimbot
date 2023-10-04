e2587ebbfe8c82045fb7a188dabee1ddfa63b265
Bernardo D'Angelo <bernardodangelo8@gmail.com>
2023-10-03 22:30:15 -0300

    fix: /regras command

--- src/main/java/com/bernardo/Season95.java
+++ src/main/java/com/bernardo/Season95.java
@@ -3,15 +3,14 @@ package com.bernardo;
 import com.bernardo.commands.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class season95 extends JavaPlugin {
+public final class Season95 extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getCommand("TextCommands").setExecutor(new TextCommands());
+        getCommand("regras").setExecutor(new TextCommands());
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
