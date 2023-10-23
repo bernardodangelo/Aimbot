399406b1da641c0714fa7fe1f18516cd845feaa0
Bernardo D'Angelo <bernardodangelo8@gmail.com>
2023-10-22 23:51:49 -0300

    refactor: update commands in main class

--- src/main/java/com/bernardo/Season95.java
+++ src/main/java/com/bernardo/Season95.java
@@ -3,22 +3,27 @@ package com.bernardo;
 import com.bernardo.commands.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class Season95 extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getCommand("regras").setExecutor(new TextCommands());
-        getCommand("twitch").setExecutor(new TextCommands());
-        getCommand("youtube").setExecutor(new TextCommands());
-        getCommand("host").setExecutor(new TextCommands());
-        getCommand("loja").setExecutor(new TextCommands());
-        getCommand("terreno").setExecutor(new TextCommands());
-        getCommand("tags").setExecutor(new TextCommands());
-        getCommand("liveon").setExecutor(new Aliases());
-        getCommand("evento").setExecutor(new Aliases());
+        Objects.requireNonNull(getCommand("regras")).setExecutor(new TextCommands());
+        Objects.requireNonNull(getCommand("twitch")).setExecutor(new TextCommands());
+        Objects.requireNonNull(getCommand("youtube")).setExecutor(new TextCommands());
+        Objects.requireNonNull(getCommand("host")).setExecutor(new TextCommands());
+        Objects.requireNonNull(getCommand("loja")).setExecutor(new TextCommands());
+        Objects.requireNonNull(getCommand("terreno")).setExecutor(new TextCommands());
+        Objects.requireNonNull(getCommand("tags")).setExecutor(new TextCommands());
+        Objects.requireNonNull(getCommand("liveon")).setExecutor(new Aliases());
+        Objects.requireNonNull(getCommand("evento")).setExecutor(new Aliases());
     }
 
     @Override
     public void onDisable() {
     }
+
+
+
 }
