eb2050a38a201df1fd5c4d5da8f9660df3f2014a
Bernardo D'Angelo <bernardodangelo8@gmail.com>
2023-10-26 19:53:19 -0300

    feat: add halloween-themed drops on entity death (halloween)

--- src/main/java/com/bernardo/Halloween.java
+++ src/main/java/com/bernardo/Halloween.java
@@ -1,11 +1,13 @@
 package com.bernardo;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.Random;
 
@@ -23,6 +25,18 @@ public class Halloween implements Listener {
                 Location deathLocation = entity.getLocation();
                 deathLocation.getWorld().strikeLightningEffect(deathLocation);
             }
+        }
+
+        if (random.nextDouble() < 0.40) {
+            event.getDrops().add(new ItemStack(Material.PUMPKIN_PIE, 1));
+        }
+
+        if (random.nextDouble() < 0.10) {
+            event.getDrops().add(new ItemStack(Material.CARVED_PUMPKIN, 1));
+        }
+
+        if (random.nextDouble() < 0.05) {
+            event.getDrops().add(new ItemStack(Material.JACK_O_LANTERN, 1));
         }
     }
 }
