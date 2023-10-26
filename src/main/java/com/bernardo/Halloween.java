a07d0964874ae69c63bde1e80bd4196b083e7e6d
Bernardo D'Angelo <bernardodangelo8@gmail.com>
2023-10-26 19:46:48 -0300

    feat: add lightning effect upon entity death (halloween)

--- src/main/java/com/bernardo/Halloween.java
+++ src/main/java/com/bernardo/Halloween.java
@@ -1,7 +1,28 @@
 package com.bernardo;
 
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+import java.util.Random;
 
 public class Halloween implements Listener {
 
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        LivingEntity entity = event.getEntity();
+        EntityType entityType = entity.getType();
+
+        Random random = new Random();
+
+        if (random.nextDouble() < 0.20) {
+            if (entityType == EntityType.ZOMBIE || entityType == EntityType.SKELETON || entityType == EntityType.SPIDER || entityType == EntityType.CREEPER) {
+                Location deathLocation = entity.getLocation();
+                deathLocation.getWorld().strikeLightningEffect(deathLocation);
+            }
+        }
+    }
 }
